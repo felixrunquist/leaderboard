c29b1489bf3c796849db88eee16aa4afaa5c9005
Felix Runquist <felix.runquist@wearebasis.com>
2025-06-09 11:37:38 +1200

    Improve docker pipeline by removing unused modules

---
+++ next.config.mjs
@@ -0,0 +1,42 @@
+import path from 'path';
+// import { BASE_URL } from './lib/constants.mjs';
+import { fileURLToPath } from 'url';
+
+import createBundleAnalyzer from '@next/bundle-analyzer'
+
+const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
+const __dirname = path.dirname(__filename); // get the name of the directory
+
+const withBundleAnalyzer = createBundleAnalyzer({
+  enabled: process.env.ANALYZE === 'true',
+})
+
+//Disable telemetry collection for privacy
+process.env.NEXT_TELEMETRY_DISABLED = 1;
+
+function composePlugins(plugins, config){
+  for(var i of plugins){
+    config = i(config)
+  }
+  return config;
+}
+
+const nextConfig = {
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'css'), path.join(__dirname, 'components')],
+  },
+  images: {
+    domains: [
+    ]
+  },
+  webpack: (config) => {
+    return config;
+  }
+}
+
+if(process.env.STANDALONE){
+  nextConfig.turbo = true;
+  nextConfig.output = 'standalone';
+}
+
+export default composePlugins([withBundleAnalyzer], nextConfig);
