2540f15a29aa9f1446b6873c459bf7af9b8a6334
Felix Runquist <felix.runquist@wearebasis.com>
2025-06-09 15:36:29 +1200

    Remove @img from standalone

--- scripts/copy-node-modules.mjs
+++ scripts/copy-node-modules.mjs
@@ -8,7 +8,7 @@ import { dirname } from 'path'
 fs.rmSync('./.next/standalone/node_modules', { recursive: true, force: true })
 fs.mkdirSync('./.next/standalone/node_modules')
 
-const rejectModules = ['sass', 'caniuse-lite', 'color', 'color-convert', 'color-name', 'color-string', 'sharp'] // Modules that should be ignored
+const rejectModules = ['sass', 'caniuse-lite', 'color', 'color-convert', 'color-name', 'color-string', 'sharp', '@img'] // Modules that should be ignored
 
 //Load JSON
 const text = fs.readFileSync('./.next/next-server.js.nft.json')
