7a921819f878bb3a29cad6fad35445e206895b52
Felix Runquist <felix.runquist@wearebasis.com>
2025-06-13 11:04:20 +1200

    Update prettierrc

---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+module.exports =
+    {
+        tabWidth: 4,
+        useTabs: false,
+        printWidth:
+            -1, // Increase to avoid wrapping
+        bracketSameLine: true, // Optional: Keep closing brackets on same line where possible
+        singleQuote: true, // Optional: Consistent with your example
+    };
